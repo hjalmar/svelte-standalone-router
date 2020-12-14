89b05c7ca45da8581fc7a605343ead5bfc0ed73c
Jens Hjalmarsson <jens.hjalmarsson@gmail.com>
2020-12-14 20:09:34 +0100

    Handling internal hashrouting and hash linking within a route. Also option to opt out of resetting the scroll position on route change.

--- router.svelte
+++ router.svelte
@@ -1,6 +1,6 @@
 <script>
   import Router, { SvelteStandaloneRouterError } from './SvelteStandaloneRouter';
-  import { contexts } from './router.js';
+  import { contexts, prevLocation } from './router.js';
 
   // as default get the first value from the contexts since a Map remembers the insertion 
   // order. this works as a way to fallback to the first context if none is provided
@@ -11,13 +11,7 @@
   }
   const { component } = contexts.get(context);
   // store the current/previous pathname to compare with the next route that wants to get loaded 
-  let prevPathname;
   context.subscribe((callback, props = {}) => {
-    // a dirty check to see it is a "component". Since there is not way to check if it is a svelte component
-    // this would atleast force it to be a function and will catch most errors where a svelte component isn't passed
-    if(typeof callback != 'function'){
-      throw new SvelteStandaloneRouterError(`Unable to load component. Did you pass a valid svelte component to the 'send' response?`);
-    }
     // implement the option to either force a reload or use the already loaded
     // component that's already been mounted at some point. By default svelte does not mount 
     // a new instance so we have to do that manually by instead of passing a svelte component,
@@ -27,14 +21,19 @@
     if(callback.hasOwnProperty('component')){
       _callback = callback.component;
     }
+    // a dirty check to see it is a "component". Since there is not way to check if it is a svelte component
+    // this would atleast force it to be a function and will catch most errors where a svelte component isn't passed
+    if(typeof _callback != 'function'){
+      throw new SvelteStandaloneRouterError(`Unable to load component. Did you pass a valid svelte component to the 'send' response?`);
+    }
     // check if the current url pathname is the same and it's not forced. if it's the same don't do anything
     // because we are already on the current page. If we force it we want it to reload.
-    if(location.pathname == prevPathname && callback.force != true){
+    if(location.pathname == prevLocation.pathname && prevLocation.firstLoad && callback.force != true){
       // we end early so we don't update the store
       return;
     }
     _callback = class extends _callback{};
-    prevPathname = location.pathname;
+    // prevPathname = location.pathname;
     // update the writable store
     component.set({
       context: _callback,
