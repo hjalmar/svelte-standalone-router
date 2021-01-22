c6c4233bce8609bc404e75e73c9d6bd98cc4691c
Jens Hjalmarsson <jens.hjalmarsson@gmail.com>
2021-01-22 13:06:16 +0100

    Added linkBase to use:ink action so that if right click we get the correct path and not the relative url which would be wrong if under a subdir

--- link.js
+++ link.js
@@ -1,4 +1,5 @@
-import { navigate, redirect, replace, alter } from './helpers.js';
+import { Router } from './SvelteStandaloneRouter';
+import { navigate, redirect, replace, alter, getPathname, cleanURL } from './helpers.js';
 
 export default (element, props) => {
   props = {
@@ -7,13 +8,24 @@ export default (element, props) => {
     title: '',
     ...props
   };
+
+  if(Router.linkBase){
+    // first we need to clean the url and add the linkbase. Thats so one can right click and
+    // open the page in a new tab or window and go to the right url
+    const clean = cleanURL(element.getAttribute('href') || '');
+    element.setAttribute('href', clean);
+  }
+  
   const clickHandler = (e) => {
     // make the check before preventing default behaviour since we should not block 
     // the default behaviour if we don't supply the required url string
-    const url = props.to || props.href || e.currentTarget.getAttribute('href') || e.currentTarget.getAttribute('data-href');
+    let url = props.to || props.href || e.currentTarget.getAttribute('href') || e.currentTarget.getAttribute('data-href');
     if(!url){
       return;
     }
+    // here we need to only get the pathname without any linkbase
+    // because that is handled in the navigational helpers
+    url = getPathname(url);
     e.preventDefault();
     if(props.type == 'navigate'){
       navigate(url, props.state, props.title);
